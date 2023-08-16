694d59cca3d7348427f8d4fd31401599f2157fd8
Ricardo Freitas <rikrdosfreitas@yahoo.com.br>
2023-08-16 13:26:30 -0300

    Update InstanceCreator.cs

--- InstanceCreator.cs
+++ InstanceCreator.cs
@@ -1,49 +1,37 @@
-public class InstanceCreator
+public class DefaultValueSetter
 {
-    public static object CreateInstanceWithDefaults(Type type)
+    public static object CreateInstanceWithDefaultValues(Type type)
     {
         if (type == null)
-        {
             throw new ArgumentNullException(nameof(type));
-        }
-
-        // Check if the type has a default (parameterless) constructor
-        if (type.GetConstructor(Type.EmptyTypes) == null)
-        {
-            throw new InvalidOperationException($"Type {type.Name} does not have a parameterless constructor.");
-        }
 
         object instance = Activator.CreateInstance(type);
 
-        // Set default values for properties with private setters
-        PropertyInfo[] properties = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-        foreach (PropertyInfo property in properties)
+        foreach (PropertyInfo property in type.GetProperties())
         {
             if (property.CanWrite)
             {
-                // If property has a private setter
-                MethodInfo setter = property.GetSetMethod(nonPublic: true);
-                if (setter != null)
-                {
-                    object defaultValue = GetDefaultValue(property.PropertyType);
-                    property.SetValue(instance, defaultValue);
-                }
+                object defaultValue = GetDefaultValue(property.PropertyType);
+                property.SetValue(instance, defaultValue);
             }
         }
 
         return instance;
     }
 
-    private static object GetDefaultValue(Type type)
+    private static object GetDefaultValue(Type propertyType)
     {
-        if (type.IsValueType)
+        if (propertyType.IsValueType)
         {
-            return Activator.CreateInstance(type);
+            return Activator.CreateInstance(propertyType);
+        }
+        else if (propertyType == typeof(string))
+        {
+            return string.Empty;
         }
         else
         {
-            return null;
+            return CreateInstanceWithDefaultValues(propertyType);
         }
     }
 }
